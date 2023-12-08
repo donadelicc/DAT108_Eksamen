5852e678f9716b25fe9933efa5d93fafab5bf445
Preben Andersen <preb1anders1@gmail.com>
2023-12-08 15:07:27 +0100

    mangler JSTL funksjonalitet

---
+++ spring_template/src/main/java/com/example/demo/ControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerTest {
+
+    @GetMapping("/test")
+    public String test_metode(Model model) {
+        String item = "Dette er en eksempeltekst for item.";
+        model.addAttribute("item", item);
+        return "tester";
+    }
+	
+	
+}
